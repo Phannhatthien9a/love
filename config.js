23bba509c71704f778182ff581801fba2c1a996d
ThienGiang234 <phannhatthien.9a@gmail.com>
2024-04-21 23:34:28 +0700

    update

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "VALENTINE Thiện yêu Giang",
+    introTitle: `Chào công chúa nhỏ bé của Anh🥰`,
+    introDesc: `Mới xa em một tí anh đã nhớ em da diết rồi . Anh chỉ ước lúc nào cũng được ở bên cạnh em. Cuộc sống của anh trở nên hoàn hảo hơn kể từ khi có em ở bên cạnh. Cảm ơn em vì tất cả và chúc em Valentine ấm áp, vui vẻ và luôn yêu anh như bây giờ nhé! Happy Valentine's Day!`,
+    btnIntro: '^^Yêu bé Giang^^',
+    title: `Happy Valentine's Day!🥰`,
+    desc: 'Bé có yêu anh Thiện nhiều hongggggg ? ',
+    btnYes: 'Bé yêu anh nhiều lắm 🥰',
+    btnNo: 'Không yêu anh đâu, liu liu',
+    question: 'Tại sao bé lại yêu anh nhiều vậy <3',
+    btnReply: 'Gửi cho anh Thiện',
+    reply: 'Tại vì anh đẹp trai quá à, hí hí, bé sẽ yêu anh mãi mãi cả đời lun nhaaaaaa',
+    mess: 'Anh biết mà 🥰. Anh cũng yêu em rất nhiều nha 😘😘',
+    messDesc: 'Nhắn tin nói chuyện với anh nha bé eoooooo.',
+    btnAccept: 'Okiiiii anh lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100017627177485' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
---
+++ config_birthday.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em Giang 23-04';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard =  "Có một ngày đặc biệt trong tháng 4, em có nhớ khum?\nLà ngày bé iu của anh đón sinh nhật lần thứ 23 đó,\nReng reng reng Happy birthday to Bé Giang iu❤️,Em yêu của anh là một người đặc biệt, một cô gái đáng yêu và quyến rũ, với trái tim ấm áp và tình yêu chân thành.\nAnh không thể tưởng tượng một ngày nào mà không có em bên cạnh.\nVà anh muốn em biết rằng, anh yêu em rất nhiều, hơn cả những từ ngữ có thể diễn tả.\nAnh yêu em từ trái tim, từ tâm hồn, từ thế giới bên trong anh, và sẽ luôn yêu em mãi mãi.\nChúc Em yêu của anh một sinh nhật đầy ý nghĩa và đáng nhớ, một ngày đầy tình yêu và hạnh phúc.\nAnh yêu em rất nhiều Trà Lê Giang❤️❤️❤️❤️❤️";
+
+
+const giftImage = 'images/p7.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
